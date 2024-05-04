31df209cc293467309d703667e0b4b9ec96653bb
leandroteo <leandro.teo88@gmail.com>
2024-05-04 11:37:37 -0300

    primeiro commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+package org.example
+fun main() {
+
+}
+/*
+GIT -
+GITHUB-(nuvem)
+Repositorio: Pasta
+branch: cópia do código
+git version
+commit (estabelecer mudanças e notificar)
+get
+
+
+
+ */
